eff51fb3d9623c63ccc33899f970ecb5116971c6
pepsipotty <smkkhanz@gmail.com>
2025-07-18 23:32:42 -0400

    replace public S3 policy with CloudFront exclusive access using OAC

--- main.tf
+++ main.tf
@@ -1,7 +1,6 @@
 provider "aws" {
   region = "us-east-1"  
 }
-
 import {
   to = aws_cloudfront_distribution.cloud-resume-cf
   id = "E30SE787VM1OZF"
@@ -17,30 +16,36 @@ resource "aws_s3_bucket" "resume_site" {
 }
 
 
-resource "aws_s3_bucket_website_configuration" "resume_site" {
-  bucket = aws_s3_bucket.resume_site.bucket
+# {
+#   "Version": "2012-10-17",           // Always this for modern policies
+#   "Statement": [{
+#     "Effect": "Allow",              // Allow or Deny
+#     "Principal": { WHO },            
+#     "Action": "WHAT_ACTION",         
+#     "Resource": "WHICH_RESOURCES",   
+#     "Condition": { WHEN }           
+#   }]
+# }
+# 
 
-  index_document {
-    suffix = "index.html"
-  }
-
-  error_document {
-    key = "404.html"
-  }
-}
-
-resource "aws_s3_bucket_policy" "public_read_access" {
+resource "aws_s3_bucket_policy" "cloudfront_access" {
   bucket = aws_s3_bucket.resume_site.id
 
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
       {
-        Sid       = "PublicReadGetObject"
-        Effect    = "Allow"
-        Principal = "*"
-        Action    = "s3:GetObject"
-        Resource  = "${aws_s3_bucket.resume_site.arn}/*"
+        Effect = "Allow"
+        Principal = {
+          Service = "cloudfront.amazonaws.com"
+        }
+        Action   = "s3:GetObject"
+        Resource = "${aws_s3_bucket.resume_site.arn}/*"
+        Condition = {
+          StringEquals = {
+            "AWS:SourceArn" = aws_cloudfront_distribution.cloud-resume-cf.arn
+          }
+        }
       }
     ]
   })
@@ -51,13 +56,14 @@ resource "aws_cloudfront_distribution" "cloud-resume-cf" {
   anycast_ip_list_id              = null
   comment                         = "cloudfront distribution for my personal resume"
   continuous_deployment_policy_id = null
-  default_root_object             = null
+  default_root_object             = "index.html"
   enabled                         = true
   http_version                    = "http2"
   is_ipv6_enabled                 = true
   price_class                     = "PriceClass_All"
   retain_on_delete                = false
   staging                         = false
+  
   tags = {
     Name = "cloud-resume-cf"
   }
@@ -79,7 +85,7 @@ resource "aws_cloudfront_distribution" "cloud-resume-cf" {
     realtime_log_config_arn    = null
     response_headers_policy_id = null
     smooth_streaming           = false
-    target_origin_id           = "sengwei-resume-site-2025.s3-website-us-east-1.amazonaws.com-md536837hkw"
+    target_origin_id           = "resume-s3-bucket-origin"
     trusted_key_groups         = []
     trusted_signers            = []
     viewer_protocol_policy     = "redirect-to-https"
@@ -88,20 +94,9 @@ resource "aws_cloudfront_distribution" "cloud-resume-cf" {
     }
   }
   origin {
-    connection_attempts      = 3
-    connection_timeout       = 10
-    domain_name              = "sengwei-resume-site-2025.s3-website-us-east-1.amazonaws.com"
-    origin_access_control_id = null
-    origin_id                = "sengwei-resume-site-2025.s3-website-us-east-1.amazonaws.com-md536837hkw"
-    origin_path              = null
-    custom_origin_config {
-      http_port                = 80
-      https_port               = 443
-      origin_keepalive_timeout = 5
-      origin_protocol_policy   = "http-only"
-      origin_read_timeout      = 30
-      origin_ssl_protocols     = ["SSLv3", "TLSv1", "TLSv1.1", "TLSv1.2"]
-    }
+    domain_name = aws_s3_bucket.resume_site.bucket_regional_domain_name
+    origin_id = "resume-s3-bucket-origin"
+    origin_access_control_id = aws_cloudfront_origin_access_control.cloud_resume_s3_oac.id
   }
   restrictions {
     geo_restriction {
@@ -113,6 +108,22 @@ resource "aws_cloudfront_distribution" "cloud-resume-cf" {
     acm_certificate_arn            = aws_acm_certificate.cloud-resume-cert.arn
     ssl_support_method             = "sni-only"
   }
+}
+
+# resource "aws_cloudfront_origin_access_control" "example" {
+#   name                              = "example"
+#   description                       = "Example Policy"
+#   origin_access_control_origin_type = "s3"
+#   signing_behavior                  = "always"
+#   signing_protocol                  = "sigv4"
+# }
+
+resource "aws_cloudfront_origin_access_control" "cloud_resume_s3_oac" {
+  name                              = "cloud_resume_s3_oac"
+  description                       = "OAC for s3 bucket"
+  origin_access_control_origin_type = "s3"
+  signing_behavior                  = "always"
+  signing_protocol                  = "sigv4"
 }
 
 resource "aws_route53_zone" "cloud-resume-hz" {
