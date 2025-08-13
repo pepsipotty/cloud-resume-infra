502952decd708aa8ea305374206d31feb4e10ffd
pepsipotty <smkkhanz@gmail.com>
2025-08-13 16:57:55 -0400

    email and visitor counter

--- main.tf
+++ main.tf
@@ -92,6 +92,22 @@ resource "aws_cloudfront_distribution" "cloud-resume-cf" {
     grpc_config {
       enabled = false
     }
+    function_association {
+      event_type   = "viewer-request"
+      function_arn = aws_cloudfront_function.url_rewrite.arn
+    }
+  }
+  
+  custom_error_response {
+    error_code         = 404
+    response_code      = 200
+    response_page_path = "/404.html"  
+  }
+
+  custom_error_response {
+  error_code         = 403
+  response_code      = 200  
+  response_page_path = "/index.html"
   }
   origin {
     domain_name = aws_s3_bucket.resume_site.bucket_regional_domain_name
@@ -110,6 +126,40 @@ resource "aws_cloudfront_distribution" "cloud-resume-cf" {
   }
 }
 
+resource "aws_cloudfront_function" "url_rewrite" {
+  name    = "add-trailing-slash"
+  runtime = "cloudfront-js-1.0"
+  comment = "Add trailing slash to URLs for Hugo"
+  publish = true
+
+  code = <<-EOT
+    function handler(event) {
+        var request = event.request;
+        var uri = request.uri;
+        
+        var response_feed = {
+            statusCode: 301,
+            statusDescription: "Moved Permanently",
+            headers: {
+                "location": { "value": "/index.xml" }
+            }
+        }
+        
+        if (uri === "/feed" || uri === "/feed/") {
+            return response_feed;
+        }
+        
+        if (uri.endsWith('/')) {
+            request.uri += 'index.html';
+        }
+        else if (!uri.includes('.')) {
+            request.uri += '/index.html';
+        }
+        
+        return request;
+    }
+EOT
+}
 # resource "aws_cloudfront_origin_access_control" "example" {
 #   name                              = "example"
 #   description                       = "Example Policy"
@@ -154,9 +204,70 @@ resource "aws_route53_record" "root" {
   }
 }
 
+// custom email domain
+resource "aws_route53_record" "email" {
+  zone_id = aws_route53_zone.cloud-resume-hz.zone_id
+  name    = ""  
+  type    = "MX"
+  ttl     = 300
+
+  records = [
+    "10 mx01.mail.icloud.com.",
+    "10 mx02.mail.icloud.com."
+  ]
+}
+resource "aws_route53_record" "email_spf" {
+  zone_id = aws_route53_zone.cloud-resume-hz.zone_id
+  name    = ""  
+  type    = "TXT"
+  ttl     = 300
+
+  records = [
+    "v=spf1 include:icloud.com ~all",
+    "apple-domain=oByg7zrKFA5O9Grn"
+  ]
+}
+resource "aws_route53_record" "email_dkim" {
+  zone_id = aws_route53_zone.cloud-resume-hz.zone_id
+  name    = "sig1._domainkey"
+  type    = "CNAME"
+  ttl     = 300
+
+  records = [
+    "sig1.dkim.sengweiyeoh.com.at.icloudmailadmin.com."
+  ]
+}
+
 resource "aws_acm_certificate" "cloud-resume-cert" {
   
   domain_name       = "sengweiyeoh.com"
   validation_method = "DNS"
   subject_alternative_names = ["www.sengweiyeoh.com"]
+}
+
+  resource "aws_dynamodb_table" "visitor_counter" {
+    name           = "resume-visitor-counter"
+    billing_mode   = "PAY_PER_REQUEST"
+    hash_key = "id"
+    attribute {
+      name = "id"                            
+      type = "S"                            
+    }
+  }
+
+  resource "aws_dynamodb_table_item" "visitor_count_initialization" {
+  table_name = aws_dynamodb_table.visitor_counter.name
+  hash_key   = aws_dynamodb_table.visitor_counter.hash_key
+
+  item = jsonencode({
+    id = {
+      S = "visitor_count"
+    }
+    count = {
+      N = "0"
+    }
+  })
+  lifecycle {
+    ignore_changes = [item]  
+  }
 }
